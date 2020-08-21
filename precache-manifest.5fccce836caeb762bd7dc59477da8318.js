68e424a8f7b982033b7207d00bc237c6e0cd8df6
toriecr <torie.cr@gmail.com>
2020-08-20 18:41:35 -0700

    Updates

---
+++ precache-manifest.5fccce836caeb762bd7dc59477da8318.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aab20109a535c6768c3",
+    "url": "/react-lights-out/static/css/main.090dacc2.chunk.css"
+  },
+  {
+    "revision": "4aab20109a535c6768c3",
+    "url": "/react-lights-out/static/js/main.4aab2010.chunk.js"
+  },
+  {
+    "revision": "a6f3a2216de1c9165847",
+    "url": "/react-lights-out/static/js/1.a6f3a221.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "2d21ea3c25c9135eaaf3e61362eb7ae1",
+    "url": "/react-lights-out/index.html"
+  }
+];
